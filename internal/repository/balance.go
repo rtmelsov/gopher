4752b036579cf82817d1cd137dafbf5f2df724c4
RahatMelsov <rahatmelsovforjob@gmai.com>
2025-03-25 10:36:08 +0500

    fix

--- internal/repository/balance.go
+++
@@ -1,7 +0,0 @@
-package repository
-
-import "github.com/rtmelsov/GopherMart/internal/models"
-
-func (r *Repository) GetBalance(id *uint) (*models.Balance, *models.Error) {
-	return r.db.GetBalance(id)
-}
